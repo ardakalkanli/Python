190bd43eb0ded7625c0cdb2b6f1ddd9488d5b3bc
ardakalkanli <kalkanliarda07@gmail.com>
2024-10-04 22:52:37 +0300

    payton dosyaları

---
+++ cerez.py
@@ -0,0 +1,15 @@
+yazar =["Anan", "Baban", "Deden"]
+aranan = input("yazar ver:")
+board= False
+
+for a in yazar:
+    if a.upper == aranan.upper:
+        board= True
+
+
+
+if flag
+
+
+else:print("yok kardeş")
+    

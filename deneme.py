fe1a176f1747211926a5adf32d3d7d89527cc4ec
ardakalkanli <kalkanliarda07@gmail.com>
2024-10-12 20:16:13 +0300

    yenilendi

--- deneme.py
+++
@@ -1,5 +0,0 @@
-with open("C:\\Users\\deNcy\\OneDrive\\Masaüstü\\payton\\writing.txt","r") as f:
-    print(f.readlines())
-    
-    
-    
--- py.py
+++
@@ -1,4 +0,0 @@
-with open("text.txt","r") as f:
-    f.readlines()
-
-   
